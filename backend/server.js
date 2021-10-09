91f894e28a4e89b64341a23073a7a1bd52a9519d
10mojijom01 <10mojijom01@gmail.com>
2021-10-09 18:19:30 +0330

    backend 1.0.1

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+// importing
+import express from 'express'
+import  mongoose  from 'mongoose'
+
+// app config
+const app = express()
+const port = process.env.PORT || 9000
+
+//middleware
+
+// DB config
+
+const connection_url = 'mongodb+srv://admin:RVLOJw9dsKVorPb0@cluster0.uklnc.mongodb.net/MafiaChatdb?retryWrites=true&w=majority'
+
+mongoose.connect(connection_url,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+
+// ???
+
+//api routes
+app.get('/',(req,res)=>res.status(200).send('hello world'))
+
+// listen
+app.listen(port,()=>console.log(`Listening on localhost : ${port}`))
